e277505f57953ef09c2dcf9e189472738e32a460
Deepti Sharma <deepti.sharma@thoughtworks.com>
2021-07-17 12:27:06 +0530

    ds #0000 Add 10% discount on all products based on competitor price

--- src/main/kotlin/com/example/ecommerce/Application.kt
+++ src/main/kotlin/com/example/ecommerce/Application.kt
@@ -5,23 +5,38 @@ import com.example.ecommerce.domain.CartItem
 import com.example.ecommerce.domain.Price
 import com.example.ecommerce.domain.Product
 
+
 fun main() {
+    val competitorProductPrices = mutableMapOf<String, Price>()
+    val ipadPro = "Ipad Pro"
+    val cricketBat = "GM Cricket bat"
+    val heroInkPen = "Hero ink Pen"
+
+    competitorProductPrices.put(ipadPro, Price(100.0))
+//    competitorProductPrices.put(heroInkPen, Price(150.0))
+    competitorProductPrices.put(cricketBat, Price(200.0))
+
     val cart = Cart()
     val newCart = Cart()
-    val price = Price(100.11)
 
-    val ipadProItem = CartItem(Product("Ipad Pro",price), 1)
-    val heroInkPenItem = CartItem(Product("Hero ink Pen",price), 1)
-    val gmCricketBatItem = CartItem(Product("GM Cricket bat",price), 2)
+    val ipadProProduct = Product(ipadPro, price(competitorProductPrices, ipadPro, 100.0))
+    val heroInkPenProduct = Product(heroInkPen, price(competitorProductPrices, heroInkPen, 50.0))
+    val cricketBatProduct = Product(cricketBat, price(competitorProductPrices, cricketBat, 900.0))
 
-    cart.addProduct(ipadProItem)
-    cart.addProduct(heroInkPenItem)
-    cart.addProduct(gmCricketBatItem)
+    val ipadProItem = CartItem(ipadProProduct, 1)
+    cart.addProduct(CartItem(ipadProProduct, 1))
+
+    cart.addProduct(CartItem(heroInkPenProduct, 1))
+    cart.addProduct(CartItem(cricketBatProduct, 2))
 
     newCart.addProduct(ipadProItem)
 
     cart.removeProduct(ipadProItem)
 
+    printCart(cart, newCart)
+}
+
+private fun printCart(cart: Cart, newCart: Cart) {
     println("Cart ${cart.cartId}")
     println("Cart ${newCart.cartId}")
 
@@ -29,4 +44,9 @@ fun main() {
 
     println("Removed items:  ${cart.getDomainEvents()}")
     println("Removed items:  ${newCart.getDomainEvents()}")
+}
+
+private fun price(competitorProductPrices: MutableMap<String, Price>, productName: String, productPrice: Double): Price {
+    val price = competitorProductPrices.get(productName)?.amount
+    return if (price == null) Price(productPrice) else Price(price - (price * 0.1))
 }
--- src/main/kotlin/com/example/ecommerce/domain/Price.kt
+++ src/main/kotlin/com/example/ecommerce/domain/Price.kt
@@ -2,7 +2,7 @@ package com.example.ecommerce.domain
 
 import java.util.*
 
-class Price(private val amount: Double, private val currency: Currency = Currency.getInstance("INR")) {
+class Price(val amount: Double, private val currency: Currency = Currency.getInstance("INR")) {
     override fun toString(): String  = "Price(amount=$amount, currency=$currency)"
 
 }
