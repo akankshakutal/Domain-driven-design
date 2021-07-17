5665db41195172d596538bc681092560a3c9a652
Deepti Sharma <deepti.sharma@thoughtworks.com>
2021-07-17 10:34:29 +0530

    ds #0000 log Product/Item was deleted from Cart

--- src/main/kotlin/com/example/ecommerce/Application.kt
+++ src/main/kotlin/com/example/ecommerce/Application.kt
@@ -8,7 +8,6 @@ fun main() {
     println("Starting")
 
     val cart = Cart()
-    val removedItem = mutableListOf<CartItem>()
 
     val ipadProItem = CartItem(Product("Ipad Pro"), 1)
     val heroInkPenItem = CartItem(Product("Hero ink Pen"), 1)
@@ -18,18 +17,9 @@ fun main() {
     addProductToCart(heroInkPenItem, cart)
     addProductToCart(gmCricketBatItem, cart)
 
-    removeItemFromCart(cart, ipadProItem, removedItem)
+    cart.removeProduct(ipadProItem)
     println("Items in cart ${cart.cartItems}")
-    removedItem.forEach { println("Removed item: ${it.product}") }
-}
-
-private fun removeItemFromCart(
-    cart: Cart,
-    itemToRemove: CartItem,
-    removedItem: MutableList<CartItem>
-) {
-    cart.removeProduct(itemToRemove)
-    removedItem.add(itemToRemove)
+    println(" Removed Items in cart ${cart.removedCartItems}")
 }
 
 
--- src/main/kotlin/com/example/ecommerce/domain/Cart.kt
+++ src/main/kotlin/com/example/ecommerce/domain/Cart.kt
@@ -2,9 +2,13 @@ package com.example.ecommerce.domain
 
 class Cart {
     val cartItems: MutableList<CartItem> = mutableListOf()
+    val removedCartItems: MutableList<CartItem> = mutableListOf()
 
     fun addProduct(cartItem: CartItem)  = cartItems.add(cartItem)
 
-    fun removeProduct(cartItem: CartItem)  = cartItems.remove(cartItem)
+    fun removeProduct(cartItem: CartItem)  {
+        cartItems.remove(cartItem)
+        removedCartItems.add(cartItem)
+    }
 
 }
